cc8668c06dece28a4f78531466639168a7480bca
vgitv <33827923+vgitv@users.noreply.github.com>
2023-03-17 11:22:57 +0100

    vlc floatting

--- config.h
+++ config.h
@@ -30,6 +30,7 @@ static const Rule rules[] = {
 	 */
 	/* class       instance    title       tags mask     isfloating   monitor */
 	{ "copyq",     NULL,       NULL,       0,            1,           -1 },
+	{ "vlc",       NULL,       NULL,       0,            1,           -1 },
 	// { "Gimp",      NULL,       NULL,       0,            0,           -1 },
 	// { "Alacritty", NULL,       NULL,       0,            1,           -1 },
 	// { "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
